3a350a32e55e23d25c8fb398b07b031bf4cdd831
Martin Jul <martin@mjul.com>
2012-05-11 12:56:50 +0200

    fixed typo.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject communityday "1.0.0"
-  :description "Code for my presentqation for the Community Day 2012 conference."
+  :description "Code for my presentation for the Community Day 2012 conference."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [net.cgrand/parsley "0.8.0"]])
